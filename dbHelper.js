2e37a4a2f8ca91b8b1456c9c1938b7778bfd8be1
Gregory <gregoryvalkovic@gmail.com>
2020-07-23 11:47:09 +1000

    Implement guild database - Add db folder to gitignore - Create dbHelper which initialises a new guild.db - dbHelper creates a members table - dbHelper can be run directly with --init to create the db

---
+++ dbHelper.js
@@ -0,0 +1,58 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const DB_PATH = './db/guild.db';
+const INIT_FLAG = "--init"
+
+
+const main = (args) => {
+	
+	if (args.length === 3 && args[2] === INIT_FLAG) {
+		init();
+	}
+	else {
+		printHelp();
+	}
+};
+
+
+// Creates and clears a new database
+const init = () => {
+
+	let db = new sqlite3.Database(DB_PATH,
+		(error) => {
+			if (error) {
+					console.log("Error opening database");
+					handleError(error);
+			}
+		}
+	);
+	
+	db.run('DROP TABLE IF EXISTS members; ' +
+		   'CREATE TABLE members(name text, last_played text, profile_pic text',
+
+		(error) => {
+			if (error) {
+				console.log("Error dropping table");
+				handleError(error);
+			}
+		}
+	);
+
+	db.close();
+};
+
+
+const printHelp = () => {
+
+	console.log("To clear/ create a new database, " +
+	            "use the flag " + INIT_FLAG
+	);
+};
+
+
+const handleError = (error) => {
+	console.error(error.message);
+}
+
+
+main(process.argv);
